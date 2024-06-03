55f4a0fa113044febbada016fa2bd560ab28f6f3
mateenshagagi <mateen.shagagi@gmail.com>
2024-06-03 15:38:55 -0700

    initial z4 commit

---
+++ zode4.f90
@@ -0,0 +1,10 @@
+program zode4
+    ! This is a comment line; it is ignored by the compiler
+    print *, 'Hello, World!'
+
+    ! Add ExprC classes: NumC, ifleq0?, BinOps
+    ! Add interp: takes in ExprC's matches it to find what type of BinOp
+
+
+  end program zode4
+  
